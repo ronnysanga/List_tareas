5e5687949e4bd3c1dd8ec0c02dc240ec4573aaa6
Ronny <rsanga@espol.edu.ec>
2022-04-05 10:07:39 -0500

    Cambio

---
+++ src/classes/todo-list.class.js
@@ -0,0 +1,24 @@
+
+
+export class TodoList {
+
+    constructor (){
+        this.todos = [];
+    }
+
+    nuevoTodo(todo){
+        this.todos.push(todo);
+    }
+
+    eliminarTodo(){
+
+    }
+
+    marcarCompletado(id){
+
+    }
+
+    eliminarCompletados(){
+        
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import "./styles.css";
-import { Todo } from "./classes/todo.class.js";
 
+import { Todo, TodoList } from "./classes";
+// import { Todo } from "./classes/todo.class.js";
+// import { TodoList } from "./classes/todo-list.class";
+
+
+const todoList = new TodoList();
 
 const tarea = new Todo("Aprender JavaScript");
+const tarea2 = new Todo("Comprar algo");
 
-console.log(tarea);
+todoList.nuevoTodo(tarea);
+todoList.nuevoTodo(tarea2);
+
+console.log(todoList);
