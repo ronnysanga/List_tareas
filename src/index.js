2db5949135219826dbae877e9e898bb881ea2d46
Ronny <rsanga@espol.edu.ec>
2022-04-07 08:30:09 -0500

    Nuevo cambio

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "./styles.css";
 
 import { Todo, TodoList } from "./classes";
+import { crearTodoHtml } from "./js/componentes";
 // import { Todo } from "./classes/todo.class.js";
 // import { TodoList } from "./classes/todo-list.class";
 
@@ -8,9 +9,10 @@ import { Todo, TodoList } from "./classes";
 const todoList = new TodoList();
 
 const tarea = new Todo("Aprender JavaScript");
-const tarea2 = new Todo("Comprar algo");
-
 todoList.nuevoTodo(tarea);
-todoList.nuevoTodo(tarea2);
+
+tarea.completado = true;
 
 console.log(todoList);
+
+crearTodoHtml(tarea);
--- src/js/componentes.js
+++ src/js/componentes.js
@@ -1,18 +1,25 @@
-import "../css/componentes.css";
-// import webpacklogo from "../assets/img/webpack-logo.png";
 
-export const saludar = (nombre)=>{
 
-    console.log("Creando etiqueta h1");
+//referencia html
+const divTodoList = document.querySelector(".todo-list");
 
-    const h1 =  document.createElement("h1");
-    h1.innerText = `Hola, ${nombre}`;
 
-    document.body.append(h1);
+export const crearTodoHtml = ( todo ) => {
 
-    //img
-    // const img = document.createElement("img");
-    // img.src = webpacklogo;
-    // document.body.append(img);  
+    const htmlTodo = `
+    <li class="${(todo.completado)?"completed":"" }"data-id="abc">
+        <div class="view">
+            <input class="toggle" type="checkbox" ${(todo.completado)?"checke d":"" }>
+            <label>${ todo.tarea }</label>
+            <button class="destroy"></button>
+        </div>
+        <input class="edit" value="Create a TodoMVC template">
+    </li>`;
+    
+    const div = document.createElement("div");
+    div.innerHTML = htmlTodo;
 
+    divTodoList.append(div.firstElementChild);
+
+    return div.firstElementChild;
 }
